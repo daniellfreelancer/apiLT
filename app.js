65fe463cf1551aa0b384aed7927c44bf5a01eb74
Daniel Lugo <daniellugofreelancer@gmail.com>
2023-03-30 11:15:40 -0300

    update cors

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors({credentials:true,origin:'https://exuberant-pink-angelfish.cyclic.app'}));
-
+// app.use(cors({credentials:true,origin:'https://exuberant-pink-angelfish.cyclic.app'}));
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
